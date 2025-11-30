79eda98903efc52a99da7d6bcf427c3b2c07a781
Karinka_ter <tigrantervartanyan@gmail.com>
2025-11-30 17:33:07 +0300

    add error-404

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {PageOne} from "./components/pages/PageOne.tsx";
 import {PageTwo} from "./components/pages/PageTwo.tsx";
 import {PageThree} from "./components/pages/PageThree.tsx";
 import {Route, Routes} from "react-router-dom";
+import {Error404} from "./components/pages/Error404.tsx";
 
 export const App = ()=> {
   return (
@@ -17,12 +18,11 @@ export const App = ()=> {
                 <Route path='/page1' element={<PageOne/>} />
                 <Route path='/page2' element={<PageTwo/>}/>
                 <Route path='/page3' element={<PageThree/>}/>
+                <Route path='*' element={<Error404/>}/>
             </Routes>
-
-          Здесь будут кроссовки
         </div>
       </div>
-      <div className={styles.footer}>abibas 2023</div>
+      <div className={styles.footer}>abibas 2025</div>
     </div>
   );
 }
