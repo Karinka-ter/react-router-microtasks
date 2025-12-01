a7b620401086a4cd6f4d1a5e55e2c51b44f6365a
Karinka_ter <tigrantervartanyan@gmail.com>
2025-12-01 14:41:26 +0300

    refactor 3

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,7 @@
 import {useParams} from "react-router-dom";
 import {adidasArr} from "../pages/Adidas.tsx";
 import s from './modalstyle.module.css'
+import {Error404} from "../pages/Error404.tsx";
 
 type Props = {
 };
@@ -10,11 +11,21 @@ export const Modal = ({}: Props) => {
     const paramsId = Number(params.id)
     const modal = adidasArr.find(m => m.id===paramsId);
     return(
-        <div className={s.boxWrapper}>
-            <h3>{modal?.name}</h3>
-            <img className={s.picture} alt={'modal'} src={modal?.picture}/>
-            <p>{modal?.price}</p>
-            <p>{modal?.collection}</p>
-        </div>
+       <>
+           {modal?(
+               <div className={s.boxWrapper}>
+                   <h3>{modal?.name}</h3>
+                   <img className={s.picture} alt={'modal'} src={modal?.picture}/>
+                   <p>{modal?.price}</p>
+                   <p>{modal?.collection}</p>
+               </div>
+           ):(
+              <Error404/>
+           )}
+       </>
+
     )
 };
+
+
+
